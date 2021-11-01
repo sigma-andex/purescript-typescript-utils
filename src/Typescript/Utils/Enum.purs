235f5a31b3f0bc17d3527a981cdd25fc8ebbfe99
sigma-andex <sigma.andex@pm.me>
2021-11-01 17:18:50 +0000

    Fix names

--- src/Typescript/Utils/Enum.purs
+++ src/Typescript/Utils/Enum.purs
@@ -5,33 +5,33 @@ import Data.Function (applyFlipped)
 import Type.Proxy (Proxy(..))
 
 class IsMatch :: forall (e :: Type). Type -> (e -> Type) -> e -> Constraint
-class IsMatch enum enumCtr enumValue | enum -> enumCtr enumValue, enumCtr enumValue -> enum where
-  isMatch :: enum -> enumCtr enumValue -> Boolean
+class IsMatch enum enumCtr enumVal | enum -> enumCtr enumVal, enumCtr enumVal -> enum where
+  isMatch :: enum -> enumCtr enumVal -> Boolean
 
 class Enum :: forall k1 k2. (k1 -> Type) -> k1 -> k2 -> Constraint
 class Enum enumCtr enumCur enumNext | enumCtr enumCur -> enumNext where
   enumValue :: enumCtr enumCur
 
 class EnumConfig :: forall k1 k2 k3. k1 -> k2 -> k3 -> Constraint
-class EnumConfig enum enumCtr enumValue | enum -> enumCtr enumValue
+class EnumConfig enum enumCtr enumVal | enum -> enumCtr enumVal
 
 class MatchHelper :: forall k1 k2. k1 -> k2 -> Type -> Type -> Type -> Constraint
-class MatchHelper enumCtr enumValue enum output fun | enumCtr enumValue enum output -> fun where
-  matchHelper :: Proxy enumCtr -> Proxy enumValue -> Proxy enum -> output -> (enum -> (enum -> output) -> output) -> fun
+class MatchHelper enumCtr enumVal enum output fun | enumCtr enumVal enum output -> fun where
+  matchHelper :: Proxy enumCtr -> Proxy enumVal -> Proxy enum -> output -> (enum -> (enum -> output) -> output) -> fun
 
 instance MatchHelper enumCtr Unit enum output (enum -> output) where
   matchHelper _ _ _ output continuation = flip continuation (const output)
-else instance (IsMatch enum enumCtr enumValue, Enum enumCtr enumValue nextEnumValue, MatchHelper enumCtr nextEnumValue enum output nextResult) => MatchHelper enumCtr enumValue enum output (((enumCtr enumValue -> output) -> nextResult)) where
+else instance (IsMatch enum enumCtr enumVal, Enum enumCtr enumVal nextEnumValue, MatchHelper enumCtr nextEnumValue enum output nextResult) => MatchHelper enumCtr enumVal enum output (((enumCtr enumVal -> output) -> nextResult)) where
   matchHelper _ _ _ output continuation =
     let
-      handler :: (enumCtr enumValue -> output) -> nextResult
+      handler :: (enumCtr enumVal -> output) -> nextResult
       handler applyMatch =
         let
           nextCont enum otherwise =
             continuation enum
               $ \i ->
                   let
-                    matchEnumValue :: enumCtr enumValue
+                    matchEnumValue :: enumCtr enumVal
                     matchEnumValue = enumValue
                   in
                     if (isMatch i matchEnumValue) then
@@ -50,6 +50,6 @@ class Match :: forall k. k -> Type -> Type -> Constraint
 class Match enum output result | enum output -> result where
   match :: Proxy enum -> output -> result
 
-instance (EnumConfig enum enumCtr enumValue, MatchHelper enumCtr enumValue enum output fun) => Match enum output fun where
+instance (EnumConfig enum enumCtr enumVal, MatchHelper enumCtr enumVal enum output fun) => Match enum output fun where
   match :: Proxy enum -> output -> fun
-  match enumProxy output = matchHelper (Proxy :: Proxy enumCtr) (Proxy :: Proxy enumValue) enumProxy output applyFlipped
+  match enumProxy output = matchHelper (Proxy :: Proxy enumCtr) (Proxy :: Proxy enumVal) enumProxy output applyFlipped
